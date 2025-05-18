62f70ded7316a253426de29f7c899786005422e7
AryaKopet <aryasokker23@gmail.com>
2025-05-18 19:12:40 +0700

    update tampilan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import SummaryCards from "./components/SummaryCard";
+import FinanceChart from "./components/FinanceChart";
+import RecentTransactions from "./components/RecentTrancastions";
 
 export default function Home() {
   return (
     <div className="space-y-8">
       <SummaryCards />
+      <FinanceChart />
+      <RecentTransactions />
+      <br />
     </div>
   );
 }
