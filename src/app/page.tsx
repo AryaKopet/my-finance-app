d021b0f2135d3b1874fd45ca2b1d8160d7220e02
AryaKopet <aryasokker23@gmail.com>
2025-05-18 21:00:42 +0700

    refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import SummaryCards from "./components/SummaryCard";
-import FinanceChart from "./components/FinanceChart";
-import RecentTransactions from "./components/RecentTrancastions";
+import SummaryCards from "../components/SummaryCard";
+import FinanceChart from "../components/FinanceChart";
+import RecentTransactions from "../components/RecentTrancastions";
 
 export default function Home() {
   return (
